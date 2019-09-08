25dd72487750a5ed51e90c7004878b9310ce3ebb
tom <gaoqiang19514@163.com>
2019-09-08 18:11:56 +0800

    路由过渡基础示例

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+
+import "./App.css";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+
+const routes = [
+  { path: "/", name: "Home", Component: Home },
+  { path: "/about", name: "About", Component: About },
+  { path: "/contact", name: "Contact", Component: Contact }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <div>
+          {routes.map(route => {
+            return (
+              <NavLink
+                key={route.path}
+                to={route.path}
+                activeClassName="active"
+                exact
+              >
+                {route.name}
+              </NavLink>
+            );
+          })}
+        </div>
+        <div className="container">
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="page">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </div>
+      </>
+    </Router>
   );
 }
 
